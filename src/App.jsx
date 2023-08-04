19603e7a560c12fa2be40425019c8ccfe8635e8a
pnoikid <rob.pena83@gmail.com>
2023-08-03 20:20:38 -0400

    made changes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
+
+
+export default function App() {
+  const [token, setToken] = useState(null);
+  return (
+    <>
+      <Authenticate token={token} setToken={setToken} />
+      <SignUpForm token={token} setToken={setToken} />
+    </>
+  );
+}
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const SignUpForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setToken(result.token);
+    try {
+        const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup");
+const result = await response.json();
+console.log(result);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <>
+      <h2>Sign Up</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit} >
+        <label>
+          Username:{" "}
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:{" "}
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        
+        </label>
+        <button>Submit</button>
+      </form>
+    </>
+  );
+}
+export default SignUpForm;
